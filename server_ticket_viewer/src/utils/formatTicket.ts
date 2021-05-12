cdc8c1e8f047a27c758bb99bd9437f79af29f4a0
Felipe <felipejsilvacosta@gmail.com>
2021-05-12 15:06:49 +1000

    fix: formatTicket function
    formatTicket is now responsible to format the date

--- server_ticket_viewer/src/utils/formatTicket.ts
+++ server_ticket_viewer/src/utils/formatTicket.ts
@@ -8,12 +8,19 @@ interface Ticket {
     description: string;
 }
 
+const formatDate = (date: string) => {
+    const stringToDate = new Date(date);
+    const dateFormatted = stringToDate.toISOString().split('T')[0];
+
+    return dateFormatted;
+};
+
 const formatTicket = (tickets: Ticket[]) => {
     const ticketFormatted = tickets.map((ticket: Ticket) => ({
         id: ticket.id,
         requester_id: ticket.requester_id,
         status: ticket.status,
-        request_dt: ticket.created_at,
+        request_dt: formatDate(ticket.created_at),
         subject: ticket.subject,
         description: ticket.description,
     }));
